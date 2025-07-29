0dd3e72f3af81151009a18311c4b2fb5e27b9536
thedeucetrey <doug.ywdc@gmail.com>
2025-07-29 10:03:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-<<<<<<< HEAD
 const { parseISO, add } = require('date-fns');
 
 mongoose.connect(process.env.MONGODB_URI)
